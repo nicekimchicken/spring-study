5d09ff745575b61a94e86de650711fa37498fb84
nicekimchicken <cerigun111@gmail.com>
2020-05-06 02:56:47 +0900

    Initial commit

---
+++ src/test/java/guru/into/springstudy/SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.into.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
